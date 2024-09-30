6d450623223960a22abf8f8e009e2f71ede20da0
Alexander Smarus <bender@readdle.com>
2024-09-30 18:49:47 +0300

    Use `NSString` extension instead of CF function for URL form encoding

--- SimpleInstapaperKit/IKRequest.m
+++ SimpleInstapaperKit/IKRequest.m
@@ -47,12 +47,19 @@
 
 - (NSString *)URLEncode
 {
-	CFStringRef encodedStringRef = CFURLCreateStringByAddingPercentEscapes(NULL,
-																		   (__bridge CFStringRef)self,
-																		   NULL,
-																		   (__bridge CFStringRef)@"!*'();:@&=+$,/?%#[]",
-																		   kCFStringEncodingUTF8);
-	return CFBridgingRelease(encodedStringRef);
+    // https://url.spec.whatwg.org/#urlencoded-serializing
+    // The application/x-www-form-urlencoded percent-encode set contains all code points,
+    // except the ASCII alphanumeric, U+002A (*), U+002D (-), U+002E (.), and U+005F (_).
+    
+    NSMutableCharacterSet *urlFormAllowedCharacters = [NSMutableCharacterSet new];
+    [urlFormAllowedCharacters addCharactersInRange:NSMakeRange(0x2A, 1)];  // 0x2A            '*'
+    [urlFormAllowedCharacters addCharactersInRange:NSMakeRange(0x2D, 2)];  // 0x2D ... 0x2E   '-.'
+    [urlFormAllowedCharacters addCharactersInRange:NSMakeRange(0x30, 10)]; // 0x30 ... 0x39   '0123456789'
+    [urlFormAllowedCharacters addCharactersInRange:NSMakeRange(0x41, 26)]; // 0x41 ... 0x5A   'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    [urlFormAllowedCharacters addCharactersInRange:NSMakeRange(0x5F, 1)];  // 0x5F            '_'
+    [urlFormAllowedCharacters addCharactersInRange:NSMakeRange(0x61, 26)]; // 0x61 ... 0x7A   'abcdefghijklmnopqrstuvwxyz'
+    
+    return [self stringByAddingPercentEncodingWithAllowedCharacters:urlFormAllowedCharacters];
 }
 
 @end
